37a6378240f1cc69b3a20a34bcfa03edf9f05060
Nacho <nachi_aven@hotmail.com>
2022-12-13 08:23:18 -0300

    Update json2mysql_v2.py

--- json2mysql_v2.py
+++ json2mysql_v2.py
@@ -10,7 +10,7 @@ with open('YourData.json', 'w') as f:
     json.dump(data, f)
 
 
-print('ARCHIVO GUARDADO CORRECTAMENTE')
+print('ARCHIVO GUARDADO CORRECTAMENTE ')
 
 # Se Lee el archivo JSON
 file = "YourData.json"
@@ -28,27 +28,27 @@ def validate_string(val):
 
 
 print('JSON VALIDADO CORECTAMENTE')
+
+
 # Se conecta a la base de datos mysql
-con = pymysql.connect(host = 'localhost', port = 3306,user = 'root',passwd = 'p4r4guaY',db = 'json2mysql')
-cursor = con.cursor()
+try:
+    con = pymysql.connect(host = 'localhost', port = 3306,user = 'root',passwd = 'p4r4guaY',db = 'json2mysql')
+    cursor = con.cursor()
+    print('SE CONECTO CORRECTAMENTE A LA BASE DE DATOS LOCAL')
+    # Se limpia primero la tabla para evitar Claves primarias duplicadas
+    cursor.execute("TRUNCATE TABLE json2mysql")
 
-
-print('SE CONECTO CORRECTAMENTE A LA BASE DE DATOS')
-
-# Se limpia primero la tabla para evitar Claves primarias duplicadas
-cursor.execute("TRUNCATE TABLE json2mysql")
-
-# Se analiza los datos json en inserción SQL
-for i, item in enumerate(json_obj):
-    id = validate_string(item.get("id", None))
-    person = validate_string(item.get("person", None))
-    year = validate_string(item.get("year", None))
-    company = validate_string(item.get("company", None))
-# Insert SQL
-    cursor.execute("INSERT INTO json2mysql (id, person, year, company) VALUES (%s,	%s,	%s,	%s)", (id, person,	year,	company) )
-
-
-print("LOS DATOS SE GUARDARON CORRECTAMENTE")
-print("PROGRAMA FINALIZADO CON EXITO")
-con.commit()
-con.close()
+    # Se analiza los datos json en inserción SQL
+    for i, item in enumerate(json_obj):
+        id = validate_string(item.get("id", None))
+        person = validate_string(item.get("person", None))
+        year = validate_string(item.get("year", None))
+        company = validate_string(item.get("company", None))
+    # Insert SQL
+        cursor.execute("INSERT INTO json2mysql (id, person, year, company) VALUES (%s,	%s,	%s,	%s)", (id, person,	year,	company) )
+        print("LOS DATOS SE GUARDARON CORRECTAMENTE EN LA BASE DATOS LOCAL")
+        print("\n¡¡PROGRAMA FINALIZADO CON EXITO!!")
+        con.commit()
+        con.close()
+except:
+    print("ERROR AL CONECTAR CON LA BASE DE DATOS")
